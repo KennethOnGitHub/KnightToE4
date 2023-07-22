89595709801e6ca24758d14b82c79fe551fc80eb
Kenny <khnsmnplwn@gmail.com>
2023-07-22 22:00:37 +0100

    tried to make a middleness thing but it no worky and I need snoo snoo

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -17,6 +17,7 @@ public class MyBot : IChessBot
 
         Move forwardestmove = moves[0];
         int bestforwardness = 0;
+        int bestmiddleness = 0;
         foreach (Move move in moves)
         {
             int forwardness = move.TargetSquare.Rank;
@@ -24,13 +25,20 @@ public class MyBot : IChessBot
             {
                 forwardness = 7 - forwardness;
             }
+            int middleness = Math.Abs(7 - 2 * move.TargetSquare.File);
 
             if (forwardness > bestforwardness)
             {
                 bestforwardness = forwardness;
+                bestmiddleness = middleness;
                 forwardestmove = move;
-
             }
+            else if (middleness > bestmiddleness & forwardness == bestforwardness) {
+                bestforwardness = forwardness;
+                bestmiddleness = middleness;
+                forwardestmove = move;
+            }
+            //above is doo doo fucking shit, terrible code but it is near bed time and I am gonna hon snoo snoo snoo
         }
         return forwardestmove;
     }
