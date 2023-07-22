098f95a51ee196e2f98b6a4b800a4e7aaf185a41
Kenny <khnsmnplwn@gmail.com>
2023-07-22 21:41:06 +0100

    made it so it goes forwards

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,37 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+        bool BotIsWhite = board.IsWhiteToMove;
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] captures = board.GetLegalMoves(true);
+        Random rnd = new Random();
+
+        if (captures.Length > 0)
+        {
+            return captures[0];
+        }
+
+        Move forwardestmove = moves[0];
+        int bestforwardness = 0;
+        foreach (Move move in moves)
+        {
+            int forwardness = move.TargetSquare.Rank;
+            if (!BotIsWhite)
+            {
+                forwardness = 7 - forwardness;
+            }
+
+            if (forwardness > bestforwardness)
+            {
+                bestforwardness = forwardness;
+                forwardestmove = move;
+
+            }
+        }
+        return forwardestmove;
     }
 }
