c2587147bed928fe839f96b53eced73919ef1d10
Kenny <khnsmnplwn@gmail.com>
2023-07-24 16:37:27 +0100

    Renamed and added certain variables for increased clarity, added comments regarding Centreness. Changed the way initialDevelopment is calculated to not take piece value into account like how newDevelopment is calculated

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,17 +8,9 @@ public class MyBot : IChessBot
     {
         bool BotIsWhite = board.IsWhiteToMove;
         Move[] moves = board.GetLegalMoves();
-        Move[] captures = board.GetLegalMoves(true);
-        Random rnd = new Random();
-
-        /*
-        if (captures.Length > 0) //if piece can be captured then it is (capture has highest importance)
-        {
-            return captures[0];
-        }
-        */
 
         Move bestmove = moves[0];
+
         double bestDevelopmentIncrease = 0;
         foreach (Move move in moves)
         {
@@ -26,11 +18,12 @@ public class MyBot : IChessBot
             int targetCentreness = CalculateCentredness(board, move.TargetSquare);
 
             int pieceVal = pieceValues[(int)(move.MovePieceType)];
-            int pieceDevelopment = pieceVal * startCentreness;
+            //int pieceDevelopment = pieceVal * startCentreness;
             //int newDevelopment = pieceVal * targetCentreness;
+            int initialDevelopment = startCentreness;
             int newDevelopment = targetCentreness;
 
-            int developmentIncrease = newDevelopment - pieceDevelopment;
+            int developmentIncrease = newDevelopment - initialDevelopment;
 
             if (developmentIncrease > bestDevelopmentIncrease)
             {
@@ -45,11 +38,18 @@ public class MyBot : IChessBot
 
     private int CalculateCentredness(Board board, Square square)
     {
-        int rankMiddleness = (int)(3.5 - Math.Abs(square.Rank - 3.5));
-        int fileMiddleness = (int)(3.5 - Math.Abs(square.File- 3.5));
+        int rankMiddleness = (int)(3.5 - Math.Abs(square.Rank - 3.5)); //Closeness to the middle in terms of ranks
+        int fileMiddleness = (int)(3.5 - Math.Abs(square.File- 3.5)); //Closeness to middle in terms of files
 
+        int Centreness = rankMiddleness * fileMiddleness; 
+        /*Things closest to the centre have the highest centreness. Multiplying the values gives the highest difference in centreness when moving into the centre.
+         This makes the calculations for development increase work better as the AI will choose to develop pieces into the centre as that gives the greatest development
+         incresase. 
+        Previously, using addition lead it to sometimes developing pieces on the edge of the board as moving two spaces forwards increases development by the same amount 
+        as moving a piece in the middle to the centre*/
+        //Issue? The AI will never develop pieces on the leftmost and rightmost side of the board, as the increase in development score is 0.
+        
         return rankMiddleness * fileMiddleness;
-        //problem is this causes it to move things near the edge as that leads to the same development increase (1 -> 3 and 4 -> 6 even though the latter is better)
     }
 
 
