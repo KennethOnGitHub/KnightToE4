eb7af9d123871663b548927fe4c02b802b9c0892
Kenny <khnsmnplwn@gmail.com>
2023-07-25 19:08:40 +0100

    start of new branch, total overhaul. Started work on the ability to calculate into the future. Reworked how material advantage is calculated

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,22 +1,37 @@
 ﻿using ChessChallenge.API;
-using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.ExceptionServices;
 
 public class MyBot : IChessBot
 {
+    int baseMaxDepth = 4;
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    bool botIsWhite;
+
     public Move Think(Board board, Timer timer)
     {
-        bool BotIsWhite = board.IsWhiteToMove;
+        botIsWhite = board.IsWhiteToMove;
+
+          
+        /*
         Move[] moves = board.GetLegalMoves();
+        int initialAdvantage = CalculateAdvantage(board);
 
         Move bestmove = moves[0];
         int bestMoveValue = 0;
         foreach (Move move in moves)
         {
-            Piece capturedPiece = board.GetPiece(move.TargetSquare);
-            int captureValue = pieceValues[(int)capturedPiece.PieceType];
-            int moveValue = captureValue + CalculateDevelopmentIncrease(move);
+            //The captured value is calculated here as that   
+            Piece capturedPiece = board.GetPiece(move.TargetSquare); 
+            int materialIncrease = pieceValues[(int)capturedPiece.PieceType];
+
+            int newAdvantage = CalculateAdvantage(board);  
+
+            int moveValue = newAdvantage - initialAdvantage;
+
             if (moveValue > bestMoveValue)
             {
                 bestMoveValue = moveValue;
@@ -26,6 +41,51 @@ public class MyBot : IChessBot
         }
         Console.WriteLine(bestMoveValue);
         return bestmove;
+        */
+    }
+
+    private int Evaluate(Board board, int currentDepth)
+    {
+        Move[] moves = board.GetLegalMoves();
+        int[] boardValues = new int[moves.Length];
+        if (currentDepth == baseMaxDepth)
+        {
+            return CalculateAdvantage(board);
+        }
+        for (int i = 0; i < boardValues.Length; i++)
+        {
+            board.MakeMove(moves[i]);
+            boardValues[i] = Evaluate(board, currentDepth + 1);
+            board.UndoMove(moves[i]);
+        }
+        //return highest value in boardvalues if it is our turn, return lowest if it is enemy turn?
+
+    }
+    private int CalculateAdvantage(Board board)
+    {
+        int materialAdvantage = CalculateMaterialAdvantage(board);
+        int positionalAdvantage = CalculatePositionalAdvantage(board);
+        int boardValue = materialAdvantage + positionalAdvantage;
+
+        return boardValue;
+    }
+
+    private int CalculateMaterialAdvantage(Board board)
+    {
+        PieceList[] pieceListList = board.GetAllPieceLists();
+        int whiteMaterialValue = pieceListList.Take(6).Sum(list => list.Count * pieceValues[(int)list.TypeOfPieceInList]); //Sums values of first 6 lists (white pieces)
+        int blackMaterialValue = pieceListList.Skip(6).Take(6).Sum(list => list.Count * pieceValues[(int)list.TypeOfPieceInList]); //Sums up next 6 lists (black pieces)
+
+        int whiteMaterialAdvantage = whiteMaterialValue - blackMaterialValue;
+        int blackMaterialAdvantage = blackMaterialValue - whiteMaterialValue;
+        int materialAdvantage = botIsWhite ? whiteMaterialAdvantage: blackMaterialAdvantage;
+
+        return materialAdvantage;
+    }
+
+    private int CalculatePositionalAdvantage(Board board) //Refactor this and material advantage due to reused code
+    {
+        return 0;
     }
 
     private int CalculateDevelopmentIncrease(Move move)
